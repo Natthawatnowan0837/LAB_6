a42d96b63af19593ad42cf8ebd4c65fca1d532da
Jonut <Natthawatnowan@gmail.com>
2022-12-13 16:14:52 +0700

    LAB_7_1.cpp completed

---
+++ LAB_7_1.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+string func1(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += x[L-i-1];
+		i++;
+	}
+	return y;
+}
+
+string func2(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += toupper(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+string func3(string x){
+	int i = 0, L = x.size();
+	string y = "";
+	while(i < L){
+		y += tolower(x[i]);
+		i++;
+	}
+	return y;	
+}
+
+
+
+
+
+int main(){
+    string x1,x2,x3,x4 ;
+    cout << "Input text: " ;
+    cin >> x1 ;
+    x2 = func3(x1);
+    x3 = func1(x2);
+    x4 = func1(x1);
+    cout << "Reversed text: " << x3<<"\n";
+    cout << "Palindrome: ";
+    if(x2==x3){
+        cout << "Yes" ;
+    }else{
+        cout << "No" ;
+    }
+    
+}
+
