dd135cd8464f697fe3563a49a2fcf4cb05cd16db
Natthawt0837 <Natthawatnowan@gmail.com>
2022-12-16 09:23:15 +0700

    LAB6_2 complete

--- Lab_6.cpp
+++ Lab_6.cpp
@@ -1,10 +1,23 @@
-#include <iostream>
-
+#include<iostream>
 using namespace std;
 
-int main()
-{
-    cout<<"Hello World";
-
+int main(){
+    int x,i=0,y=0;
+    while (x != 0){
+         cout << "Enter an integer: ";
+         cin >> x;
+         if( x != 0){
+             if(x%2 == 0){
+                 i++;
+             }else{
+                 y++;
+             }
+         }
+         
+        
+    }
+        
+    cout << "#Even numbers = "<<i <<endl;
+    cout << "#Odd numbers = "<<y <<endl;
     return 0;
 }
